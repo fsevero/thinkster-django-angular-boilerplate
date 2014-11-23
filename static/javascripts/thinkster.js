19af71b9d20ba804e8b6c0f43c6c95b1bb7dcc9d
Severo <severo.fabricio@gmail.com>
2014-11-23 17:55:01 -0200

    Tutorial completed

---
+++ static/javascripts/thinkster.js
@@ -0,0 +1,43 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('thinkster', [
+            'thinkster.config',
+            'thinkster.routes',
+            'thinkster.authentication',
+            'thinkster.layout',
+            'thinkster.posts',
+            'thinkster.utils',
+            'thinkster.profiles'
+        ]);
+
+    angular
+        .module('thinkster.routes', ['ngRoute']);
+
+    angular
+        .module('thinkster.config', []);
+
+    angular
+        .module('thinkster.layout', []);
+
+    angular
+        .module('thinkster.posts', []);
+
+    angular
+        .module('thinkster.utils', []);
+
+    angular
+        .module('thinkster.profiles', []);
+
+    angular
+        .module('thinkster')
+        .run(run);
+
+    run.$inject = ['$http'];
+
+    function run($http) {
+        $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+        $http.defaults.xsrfCookieName = 'csrftoken';
+    }
+})();
--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,8 +1,27 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 from django.views.generic.base import TemplateView
+
+from rest_framework import routers
+from rest_framework_nested.routers import NestedSimpleRouter
+
+from authentication.views import AccountViewSet, LoginView, LogoutView
+from posts.views import AccountPostsViewSet, PostViewSet
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
+router.register(r'posts', PostViewSet)
+
+accounts_router = NestedSimpleRouter(
+    router, r'accounts', lookup='account'
+)
+accounts_router.register(r'posts', AccountPostsViewSet)
 
 urlpatterns = patterns(
     '',
+    url(r'^api/v1/auth/login/$', LoginView.as_view(), name="login"),
+    url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name="logout"),
+    url(r'^api/v1/', include(router.urls)),
+    url(r'^api/v1/', include(accounts_router.urls)),
 
-    url('^.*$', TemplateView.as_view(template_name='index.html'), name='index'),
+    url(r'^', TemplateView.as_view(template_name='index.html'), name='index'),
 )
